669845dd265e367ed3b70b48f4d3d04808b6f757
Young wook Jeon <ela87@naver.com>
2021-04-29 23:05:36 +0900

    Solve quiz1 in part1

---
+++ src/part1/ClassAnalyzer.java
@@ -0,0 +1,56 @@
+package part1;
+
+import java.util.*;
+
+public class ClassAnalyzer {
+    private static final List<String> JDK_PACKAGE_PREFIXES =
+            Arrays.asList("com.sun.", "java", "javax", "jdk", "org.w3c", "org.xml");
+
+    public static PopupTypeInfo createPopupTypeInfoFromClass(Class<?> inputClass) {
+        PopupTypeInfo popupTypeInfo = new PopupTypeInfo();
+
+        /** Complete the Code **/
+        popupTypeInfo.setPrimitive(inputClass.isPrimitive())
+                .setInterface(inputClass.isInterface())
+                .setEnum(inputClass.isEnum())
+                .setName(inputClass.getName())
+                .setJdk(isJdkClass(inputClass))
+                .addAllInheritedClassNames(getAllInheritedClassNames(inputClass));
+
+        return popupTypeInfo;
+    }
+
+    /*********** Helper Methods ***************/
+
+    public static boolean isJdkClass(Class<?> inputClass) {
+        /** Complete the code
+         Hint: What does inputClass.getPackage() return when the class is a primitive type?
+         **/
+        return JDK_PACKAGE_PREFIXES.stream()
+                .anyMatch(packagePrefix -> inputClass.getPackage() == null ||
+                        inputClass.getPackage().getName().startsWith(packagePrefix));
+    }
+
+    public static String[] getAllInheritedClassNames(Class<?> inputClass) {
+        /** Complete the code
+         Hints: What does inputClass.getSuperclass() return when the inputClass doesn't inherit from any class?
+         What does inputClass.getSuperclass() return when the inputClass is a primitve type? -> null
+         **/
+        String[] inheritedClasses;
+        if (inputClass.isInterface()) {
+            inheritedClasses = Arrays.stream(inputClass.getInterfaces())
+                    .map(Class::getSimpleName)
+                    .toArray(String[]::new);
+        } else {
+            Class<?> inheritedClass = inputClass.getSuperclass();
+            inheritedClasses = inheritedClass != null ?
+                    new String[]{inputClass.getSuperclass().getSimpleName()} : null;
+        }
+        return inheritedClasses;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(createPopupTypeInfoFromClass(List.class));
+        // PopupTypeInfo{isPrimitive=false, isInterface=true, isEnum=false, name='java.util.List', isJdk=true, inheritedClassNames=[Collection]}
+    }
+}
